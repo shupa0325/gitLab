1a61da82a8e69f811ce421fba4eda1e228de656e
Shupa <fir325vm0@gmail.com>
2016-07-29 04:09:14 +0000

    PDO Begin here

--- WorldO/core/Server.php
+++ WorldO/core/Server.php
@@ -7,4 +7,24 @@
             mysqli_set_charset(Server::$worldO, "utf8");
         }
     }
+    
+    class myServer{
+        private static $connection = NULL;
+         
+        function __construct() {
+            $pdo = new PDO("mysql:host=localhost;dbname=worldO;port=3306", "guest", "guest");
+            $pdo->exec("SET CHARACTER SET utf8");
+            self::$connection = $pdo;
+            $pdo = null;
+        }
+         
+        function getConnection(){
+            return self::$connection;
+        }
+         
+         
+        function closeConnection(){
+            self::$connection = NULL;
+        }
+    }
     ?>
